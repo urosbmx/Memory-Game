17be4fd9d2161354d024dadfcc613402627cfcfe
Uros <uros.katani@outlook.com>
2021-11-28 09:30:25 +0100

    Initial Commit

---
+++ Memory Game/Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_GameApp.swift
+//  Memory Game
+//
+//  Created by Uroš Katanić on 28.11.21..
+//
+
+import SwiftUI
+
+@main
+struct Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memory GameTests/Memory_GameTests.swift
@@ -0,0 +1,33 @@
+//
+//  Memory_GameTests.swift
+//  Memory GameTests
+//
+//  Created by Uroš Katanić on 28.11.21..
+//
+
+import XCTest
+@testable import Memory_Game
+
+class Memory_GameTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
