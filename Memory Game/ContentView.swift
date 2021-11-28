3740729e2bcd692dff6e1cddb1e0f83426f8108b
Uros <uros.katani@outlook.com>
2021-11-28 10:55:28 +0100

    Changes cards on click

--- Memory Game/ContentView.swift
+++ Memory Game/ContentView.swift
@@ -10,32 +10,49 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack{
-            CardView(valueCard: "Uros")
+            CardView(valueCard: "💻")
+            
         }
-    }
+        .padding(.horizontal)
+            }
 }
 
 
 //This is view and designe for card's
 
+
 struct CardView: View {
     var valueCard: String
+    var closeCard = false
     var body: some View{
-        ZStack{
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            Text(valueCard)
-                .font(.largeTitle)
-        }
-        .padding()
+        if closeCard {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 30)
+                        .stroke(lineWidth: 3)
+                    Text(valueCard)
+                        .font(.largeTitle)
+                }
+                .padding()
+            }else{
+                ZStack{
+                    RoundedRectangle(cornerRadius: 30)
+                        .fill()
+                        .foregroundColor(.red)
+                }
+                .padding()
+            }
     }
     
 }
+
+//onTapGesture(count: <#T##Int#>, perform: )
 
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+//            .preferredColorScheme(.dark)
+        
     }
 }
